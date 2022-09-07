f9d35bb562f1ac7f5653450fb60dd083020c57af
huang@fedora <0711feiyu@gmail.com>
2022-09-07 10:57:25 +0800

    trace: Fix issue with hitsz

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -159,17 +159,18 @@ static char sysname[][8] = {
 void
 syscall(void)
 {
-  int num;
+  int num, arg;
   struct proc *p = myproc();
 
   num = p->trapframe->a7;
+  arg = p->trapframe->a0;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
 
     // only trace sets the mask
     if (p->mask > 0 && p->mask & (1 << num))
-      printf("%d: syscall %s -> %d\n",
-              p->pid, sysname[num], (int)p->trapframe->a0);
+      printf("%d: sys_%s(%d) -> %d\n",
+              p->pid, sysname[num], arg, (int)p->trapframe->a0);
 
   } else {
     printf("%d %s: unknown sys call %d\n",
