c5cf281d14ed5c8d22353d5d48fda23807b0a79e
Feiyu Huang <0711feiyu@gmail.com>
2022-09-20 14:55:39 +0800

    pgtbl: Finish ex1
    pgtbl: Fix little
    
    Fix typo

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 > 2021 [pgtbl](https://pdos.csail.mit.edu/6.S081/2021/labs/pgtbl.html) lab
 
+## Speed up system calls
+
+Add a sharing page which stored user pid into the syscall.
+
+It's fairly easy to implement.
--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,14 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page.
+  if((p->usyspage = (struct usyscall*)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyspage->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -150,6 +158,9 @@ found:
 static void
 freeproc(struct proc *p)
 {
+  if(p->usyspage)
+    kfree((void*)p->usyspage);
+  p->usyspage = 0;
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
@@ -196,6 +207,16 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the usyscall just below TRAMFRAME, for sharing read-only data pid
+  // between kernel and user
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->usyspage), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +227,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
