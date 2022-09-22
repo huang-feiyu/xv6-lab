57391cc40b701d0c493883e2011b996006726e2b
Feiyu Huang <0711feiyu@gmail.com>
2022-09-22 15:06:03 +0800

    lock: Finish naive version of multi-freelist

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # lock
 
-> TODO:
+> In [lock](https://pdos.csail.mit.edu/6.S081/2020/labs/lock.html) lab, you'll
+> gain experience in re-designing code to increase parallelism
 
+## Memory Allocator
+
+In order to reduce contention, re-design both data structures and locking
+strategies for the xv6 memory allocator.
+
+The basic idea is to maintain a free list per CPU, each list with its own lock.
+Allocations and frees on different CPUs can run in parallel, because each CPU
+will operate on a different list.
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,22 +21,31 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  char lockname[16];
+  for(int i = 0; i < NCPU; i++){
+    memset(lockname, 0, 16); // clear buffer
+    snprintf(lockname, 16, "kmem[%d]", i);
+    initlock(&kmem[i].lock, lockname);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
+  push_off(); // turn off interrupts
+
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
+
+  pop_off(); // turn on interrupts
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,7 +55,10 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  push_off(); // turn off interrupts
+
   struct run *r;
+  int id = cpuid();
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +68,12 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+
+  pop_off(); // turn on interrupts
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +82,20 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+  push_off(); // turn off interrupts
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  struct run *r;
+  int id = cpuid();
+
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  release(&kmem[id].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+
+  pop_off(); // turn on interrupts
   return (void*)r;
 }
