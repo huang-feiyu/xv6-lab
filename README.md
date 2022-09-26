2a880b105ea6eed3e37cfecac83ec7dde28cfd12
Faye Huang <0711feiyu@gmail.com>
2022-09-26 09:59:48 +0800

    thread: Finish uthread & Pass test

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 > [thread](https://pdos.csail.mit.edu/6.S081/2020/labs/thread.html) lab will
 > familiarize you with multithreading.
 
+## Uthread: switching between threads
+
+Very similar to [M2:libco](http://jyywiki.cn/OS/2022/labs/M2).
+
+* when `thread_schedule()` runs a given thread for the first time, the thread
+  executes the function passed to `thread_create()`, on its own stack
+* `thread_switch` saves the registers of the thread being switched away from,
+  restores the registers of the thread being switched to, and returns to the
+  point in the latter thread's instructions where it last left off
+
+My design policy: steal from kernel... =>
+It is fairly easy to implement, because the skeleton has done almost everything.
--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,30 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+/* steal from proc.h */
+struct tcontext {
+  uint64 ra;
+  uint64 sp; // stack pointer, the "bottom" of stack (top on addr)
+
+  /* callee saved registers */
+  uint64 s0; // frame pointer, current
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct tcontext context;           /* the thread's context */
+  char            stack[STACK_SIZE]; /* the thread's stack */
+  int             state;             /* FREE, RUNNING, RUNNABLE */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -59,10 +78,8 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    // switch context to next thread and store old thread
+    thread_switch((uint64)&t->context, (uint64)&next_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,7 +93,9 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)&t->stack[STACK_SIZE - 1];
+  t->context.s0 = (uint64)&t->stack[STACK_SIZE - 1];
 }
 
 void
